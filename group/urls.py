42b71d1283263e64fdc4287ded42ee65e872cacc
Sai <chulkovdany1609@gmail.com>
2024-09-18 18:06:18 +0300

    Группы

--- group/urls.py
+++ group/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import groups, group
+from .views import groups, group, group_add, group_edit, group_delete
 
 urlpatterns=[
     path('', groups, name='groups'),
-    path('<slug:slug>/', group, name='group')
+    path('add/', group_add, name='group_add'),
+    path('<slug:slug>/', group, name='group'),
+    path('<slug:slug>/edit', group_edit, name='group_edit'),
+    path('<slug:slug>/delete', group_delete, name='group_delete'),
 ]
